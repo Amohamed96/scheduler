76d761a33950de841dfe3d3ccf1201133f293f29
Amohamed96 <amohamed.rg@gmail.com>
2021-12-09 12:24:11 -0500

    From Storybook to Application

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,7 +1,25 @@
 import React from "react";
-
 import "components/Application.scss";
+import DayList from "./DayList";
+import React, { useState } from "react";
 
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
 export default function Application(props) {
   return (
     <main className="layout">
@@ -10,7 +28,13 @@ export default function Application(props) {
             src="images/logo.png" 
             alt="Interview Scheduler"/>
        <hr  className="sidebar__separator sidebar--centered" />
-       <nav className="sidebar__menu"></nav>
+       <nav className="sidebar__menu">
+            <DayList
+              days={days}
+              day={"Monday"}
+              setDay={day => console.log(day)}
+            />
+       </nav>
        <img
             className="sidebar__lhl sidebar--centered"
             src="images/lhl.png"
